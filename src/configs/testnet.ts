5c34c05cbf28a11efb6492ce51a2f96f68ca3543
Trung Do <dttr278@gmail.com>
2022-12-05 09:38:53 +0700

    update config

--- src/configs/testnet.ts
+++ src/configs/testnet.ts
@@ -33,6 +33,6 @@ const configs: Configs = {
     },
   },
   DEFAULT_CHAIN: "BSC",
-  API_URL: "http://localhost:8080/",
+  API_URL: process.env.REACT_APP_API_URI || "http://localhost:8086/",
 };
 export default configs;
