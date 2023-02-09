b8b49a0d58083635cbbd979d3356a6ce8b3c66ae
Trung Do <dttr278@gmail.com>
2023-02-09 14:15:37 +0700

    move account service to market service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -9,22 +9,22 @@ import { ProfileUploadFile } from "./types/params/ProfileUploadFile";
 import { QueryAccounts } from "./types/params/QueryAccounts";
 
 const nonce = (params: any) =>
-  client.get(`/account/api/authentication/nonce`, { params });
+  client.get(`/market/api/authentication/nonce`, { params });
 const login = (body: any) =>
-  client.post(`/account/api/authentication/token`, body);
+  client.post(`/market/api/authentication/token`, body);
 
 const profile = async (): Promise<UserDto> =>
-  await client.get(`/account/api/profile`);
+  await client.get(`/market/api/profile`);
 
 const profiles = async (
   params: QueryAccounts
 ): Promise<BaseResult<PaginationDto<UserDto>>> =>
-  await client.get(`/account/api/profiles`, { params });
+  await client.get(`/market/api/profiles`, { params });
 
 const profileByAddress = async (
   address: string
 ): Promise<BaseResult<UserDto>> =>
-  await client.get(`/account/api/profile/${address}`);
+  await client.get(`/market/api/profile/${address}`);
 
 const uploadProfileFile = async (
   params: ProfileUploadFile
@@ -36,31 +36,31 @@ const uploadProfileFile = async (
   if (params.cover) {
     formData.append("cover", params.cover);
   }
-  return await client.postForm("/account/api/updateFile", formData);
+  return await client.postForm("/market/api/updateFile", formData);
 };
 const updateProfile = async (body: UserDto): Promise<UserDto> => {
-  return await client.post("/account/api/updateProfile", body);
+  return await client.post("/market/api/updateProfile", body);
 };
 
 const manageInfo = async (body: UserDto): Promise<UserDto> => {
-  return await client.post("/account/api/manageInfo", body);
+  return await client.post("/market/api/manageInfo", body);
 };
 const getNotifications = async (
   params: GetNotificationsDto
 ): Promise<BaseResult<PaginationDto<NotificationDto>>> => {
-  return await client.get("/account/api/notifications", { params });
+  return await client.get("/market/api/notifications", { params });
 };
 const updateNotificationStatus = async (
   id: string,
   status: NotificationStatus
 ): Promise<BaseResult<string>> => {
-  return await client.post("/account/api/notifications/status", {
+  return await client.post("/market/api/notifications/status", {
     id,
     status,
   });
 };
 const seenAllNotification = async () => {
-  return await client.post("/account/api/notifications/seenAll");
+  return await client.post("/market/api/notifications/seenAll");
 };
 
 export const accountService = {
