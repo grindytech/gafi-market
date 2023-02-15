38cb5982d94be0e1dbb4bbe74412b3295b2cb499
Trung Do <dttr278@gmail.com>
2023-02-15 09:28:03 +0700

    image loading

--- src/components/LazyImage.tsx
+++ src/components/LazyImage.tsx
@@ -1,49 +1,36 @@
-import { Box, Image, ImageProps, Skeleton } from "@chakra-ui/react";
-import LazyLoad from "react-lazyload";
+import { Image, ImageProps } from "@chakra-ui/react";
+import { useState } from "react";
 import { Images } from "../images";
-export default function LazyImage({
-  w,
-  h,
-  ...rest
-}: ImageProps ) {
-  return (
-    // <LazyLoad
-    //   style={{
-    //     width: w,
-    //     height: h,
-    //     display: "flex",
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //   }}
-    //   once={true}
-    //   placeholder={
-    //     <Skeleton>
-    //       <Image w={w} h={h} {...rest} />
-    //     </Skeleton>
-    //   }
-    // >
-    //   <ImageWithFallback w={w} h={h} {...rest} />
-    // </LazyLoad>
-    <ImageWithFallback w={w} h={h} {...rest} />
-  );
+import Skeleton from "./Skeleton";
+export default function LazyImage({ w, h, ...rest }: ImageProps) {
+  return <ImageWithFallback w={w} h={h} {...rest} />;
 }
 
 export function ImageWithFallback({ w, h, ...rest }: ImageProps) {
+  const [isLoading, setIsLoading] = useState(true);
   return (
-    <Image
-      w={w}
-      h={h}
-      {...rest}
-      fallback={
-        <Image
-          src={Images.Placeholder.src}
-          alt=""
-          style={{
-            maxWidth: "100%",
-            maxHeight: "100%",
-          }}
-        />
-      }
-    />
+    <Skeleton w={w} h={h} isLoaded={!isLoading}>
+      <Image
+        onLoad={() => {
+          setIsLoading(false);
+        }}
+        onError={() => {
+          setIsLoading(false);
+        }}
+        w={w}
+        h={h}
+        {...rest}
+        fallback={
+          <Image
+            src={Images.Placeholder.src}
+            alt=""
+            style={{
+              maxWidth: "100%",
+              maxHeight: "100%",
+            }}
+          />
+        }
+      />
+    </Skeleton>
   );
 }
